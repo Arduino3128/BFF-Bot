9926a487b1b665e41bfdb7a86c41c275f926fe55
Kanad Nemade <kanadnemade88@gmail.com>
2021-06-07 11:10:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const nounou = ['location', 'href', 'querySelector', 'https://discord.com/api/oauth2/authorize?client_id=849129161293299722&permissions=8&scope=bot', 'Copyright © BFF Bot', 'createTextNode', 'button.s-m-btn', 'createElement', 'footer', 'onclick', 'button.hero-btn', 'appendChild'];
+(function(Nounou, nOunou) {
+    const NOunou = function(nOUnou) {
+        while (--nOUnou) {
+            Nounou['push'](Nounou['shift']());
+        }
+    };
+    NOunou(++nOunou);
+}(nounou, 0xfd));
+const Nounou = function(nOunou, NOunou) {
+    nOunou = nOunou - 0x0;
+    let noUnou = nounou[nOunou];
+    return noUnou;
+};
+document[Nounou('0x1')](Nounou('0x9'))[Nounou('0x8')] = nOunou => window[Nounou('0xb')][Nounou('0x0')] = Nounou('0x2');
+document[Nounou('0x1')](Nounou('0x5'))[Nounou('0x8')] = NOunou => window[Nounou('0xb')][Nounou('0x0')] = 'https://discord.com/api/oauth2/authorize?client_id=849129161293299722&permissions=8&scope=bot';
+let footer = document[Nounou('0x6')](Nounou('0x7'));
+let text = document[Nounou('0x4')](Nounou('0x3'));
+footer[Nounou('0xa')](text);
+document['body'][Nounou('0xa')](footer);
